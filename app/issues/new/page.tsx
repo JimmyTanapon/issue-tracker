96de90eb1ffc654f9810b4f553ff7a9a52561e84
JimmyTanapon <paisuwan16@gmail.com>
2023-10-16 23:04:24 +0700

    Extract the error message component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -7,9 +7,10 @@ import "easymde/dist/easymde.min.css";
 import { Controller, useForm } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
-import {zodResolver} from '@hookform/resolvers/zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { CreateIssueSchma } from '@/app/validationSchema';
 import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 
 type IssueForm = z.infer<typeof CreateIssueSchma>
@@ -22,41 +23,41 @@ type IssueForm = z.infer<typeof CreateIssueSchma>
 const NewIssuePage = () => {
   const router = useRouter()
   const [error, setError] = useState('')
-  const { register, control,handleSubmit,formState:{errors}} = useForm<IssueForm>({
-    resolver:zodResolver(CreateIssueSchma)
+  const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
+    resolver: zodResolver(CreateIssueSchma)
   })
 
 
   return (
-  <div className='max-w-xl'>
-   {error && 
-   <Callout.Root color='red' className=' mb-5'>
-      <Callout.Text>{error}</Callout.Text>
-    </Callout.Root>}
-      <form className='  space-y-3' onSubmit={handleSubmit(async(data)=>{
+    <div className='max-w-xl'>
+      {error &&
+        <Callout.Root color='red' className=' mb-5'>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>}
+      <form className='  space-y-3' onSubmit={handleSubmit(async (data) => {
         try {
-          await axios.post('/api/issues',data)
+          await axios.post('/api/issues', data)
           router.push('/issues')
         } catch (error) {
           setError('An unexpected  error occurred.')
         }
-  
+
       })}>
         <TextField.Root>
           <TextField.Input placeholder='Title'  {...register('title')} />
         </TextField.Root>
-        {errors.title && <Text color='red' as='p'>{errors.title.message}</Text>}
+        {<ErrorMessage >{errors.title?.message}</ErrorMessage>}
         <Controller
           name='description'
           control={control}
-          render={({field}) => <SimpleMDE placeholder='Description here...' {...field}/>}
+          render={({ field }) => <SimpleMDE placeholder='Description here...' {...field} />}
         />
-                {errors.description && <Text color='red' as='p'>{errors.description.message}</Text>}
+        {<ErrorMessage>{errors.description?.message}</ErrorMessage>}
 
         <Button>Submit New issue</Button>
       </form>
-  </div>
- 
+    </div>
+
   )
 }
 
