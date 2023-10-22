a907158e1556b174cf9f6d883be52927ba126653
JimmyTanapon <paisuwan16@gmail.com>
2023-10-23 00:55:13 +0700

    Additional loading skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,8 +1,20 @@
+import { Heading, Flex, Card, Text, Box } from '@radix-ui/themes'
 import React from 'react'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const LoadingIssueDetailPage = () => {
   return (
-    <div>Loading...</div>
+    <Box>
+     <Skeleton  className='  max-w-xl'/>
+      <Flex className=' space-x-3 my-2'>
+        <Skeleton width={'5rem'} />
+        <Skeleton width={'8rem'}/>
+      </Flex>
+      <Card className='prose ' mt={'4'}>
+        <Skeleton count={3} />
+      </Card>
+    </Box>
   )
 }
 
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -2,7 +2,7 @@ import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import prisma from '@/prisma/client'
 import { Card, Flex, Heading, Text } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
- import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from 'react-markdown'
 import React from 'react'
 
 interface Props {
@@ -19,20 +19,21 @@ const IssueDetaillPage = async ({ params }: Props) => {
     if (!issue) {
         notFound()
     }
+  
     return (
         <div>
             <Heading >{issue.title}</Heading>
-          <Flex className=' space-x-3 my-2'>
-                 <IssueStatusBadge status={issue.status} />
-                 <Text>{issue.createAt.toDateString()}</Text>
-          </Flex>
-          <Card className='prose ' mt={'4'}>
-             <ReactMarkdown>{issue.description}</ReactMarkdown>
-          </Card>
-           
-           
+            <Flex className=' space-x-3 my-2'>
+                <IssueStatusBadge status={issue.status} />
+                <Text>{issue.createAt.toDateString()}</Text>
+            </Flex>
+            <Card className='prose ' mt={'4'}>
+                <ReactMarkdown>{issue.description}</ReactMarkdown>
+            </Card>
 
-          
+
+
+
 
 
         </div>
