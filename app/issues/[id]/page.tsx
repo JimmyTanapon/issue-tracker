5f67bfc8343fa95073dc636f2ad65f0d8f19fbfb
JimmyTanapon <paisuwan16@gmail.com>
2023-11-03 23:06:45 +0700

    Optimizing performance useing React Cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,7 +3,7 @@ import prisma from '@/prisma/client'
 import { Box, Flex, Grid } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 
-import React from 'react'
+import React, { cache } from 'react'
 
 import IssueDetails from './IssueDetails'
 import EditissueButton from './EditIssueButton'
@@ -15,28 +15,27 @@ import AssigneeSelect from './AssigneeSelect'
 interface Props {
     params: { id: string }
 }
+const fetchUser = cache( (issueId: number) =>  prisma.issue.findUnique({where: { id:issueId } }))
 
 const IssueDetaillPage = async ({ params }: Props) => {
     const session = await getServerSession()
 
     // if(typeof params.id !== 'number') notFound();
 
-    const issue = await prisma.issue.findUnique({
-        where: { id: parseInt(params.id) }
-    })
+    const issue = await fetchUser(parseInt(params.id))
     if (!issue) {
         notFound()
     }
 
     return (
         <Grid columns={{ initial: '1', sm: '5' }} gap={'5'}>
-    
+
             <Box className='md:col-span-4 s'>
                 <IssueDetails issue={issue} />
             </Box>
             {session && (<Box>
                 <Flex direction={'column'} gap={'4'}>
-                    <AssigneeSelect issue={issue}/>
+                    <AssigneeSelect issue={issue} />
                     <EditissueButton issueId={issue.id} />
                     <DeleteIssueButton issueId={issue.id} />
                 </Flex>
@@ -46,12 +45,10 @@ const IssueDetaillPage = async ({ params }: Props) => {
 }
 
 export default IssueDetaillPage
-export async function generateMetadata({params}:Props){
-    const issue =await prisma.issue.findUnique({
-             where:{id:parseInt(params.id)}
-     })
-     return {
-         title:issue?.title,
-         discription:'Details of  issue'+ issue?.id
-     }
- }
+export async function generateMetadata({ params }: Props) {
+    const issue = await fetchUser(parseInt(params.id))
+    return {
+        title: issue?.title,
+        discription: 'Details of  issue' + issue?.id
+    }
+}
