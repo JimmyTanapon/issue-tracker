250923f10fa62cfc4615a8fbd62aa06aa22aaa58
JimmyTanapon <paisuwan16@gmail.com>
2023-11-05 19:43:19 +0700

    DeleteIssueButton no't working

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -10,7 +10,7 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     const [error, setError] = useState(false)
     const [isDeleting, setIsDeleting] = useState(false)
 
-    const  deleteIssue = async () => {
+    const deleteIssue = async () => {
         try {
             setIsDeleting(true)
             await axios.delete(`/api/issues/${issueId}`)
@@ -27,10 +27,10 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
         <>
             <AlertDialog.Root>
                 <AlertDialog.Trigger>
-                    <Button color='red' disabled={isDeleting}>
-                        {isDeleting && <Spinner/>}
+                    <Button  disabled={isDeleting}>
+                        {isDeleting && <Spinner />}
                         Delete Issue
-                        </Button>
+                    </Button>
                 </AlertDialog.Trigger>
                 <AlertDialog.Content>
                     <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
@@ -40,7 +40,7 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
                             <Button variant='soft' color='gray'>Cancel</Button>
                         </AlertDialog.Cancel>
                         <AlertDialog.Action>
-                            <Button color='red' onClick={deleteIssue}>Delete Issue</Button>
+                            <Button color='red'  variant='soft' onClick={deleteIssue}>Delete Issue</Button>
                         </AlertDialog.Action>
                     </Flex>
                 </AlertDialog.Content>
@@ -51,7 +51,7 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
                     <AlertDialog.Title>Error</AlertDialog.Title>
                     <AlertDialog.Description>This issue could not be  deleted.</AlertDialog.Description>
                     <AlertDialog.Cancel>
-                        <Button variant='soft' color='gray' mt={'2'} onClick={()=>setError(false)}>OK</Button>
+                        <Button variant='soft' color='gray' mt={'2'} onClick={() => setError(false)}>OK</Button>
                     </AlertDialog.Cancel>
 
 
